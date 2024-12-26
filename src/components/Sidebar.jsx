afb3a2b9a1732e4ed658ee1a32d76d2f9190c21d
Hamza Shadeez <hahmad1178@gmail.com>
2024-12-26 13:04:56 +0100

    route

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,32 +12,32 @@ const Sidebar = () => {
       <img src="/logo.svg" alt="" className="w-3/4 mt-4" />
       <div className="mt-4 flex flex-col gap-8  pt-4 md:pt-16 md:px-5">
         <Link
-          to={"/chats"}
+          to={"/dashboard"}
           className="flex gap-4 items-center hover:text-brand hover:font-bold "
         >
           <MdDashboard size={30} color="gray" />
-          <h3 className="text-xl font-semibold">Dashboard</h3>
+          <h3 className="text-xl hidden md:flex font-semibold">Dashboard</h3>
         </Link>
         <Link
           to={"/bookings"}
           className="flex gap-4 items-center hover:text-brand hover:font-bold "
         >
           <IoTime size={30} color="gray" />
-          <h3 className="text-xl font-semibold">Bookings</h3>
+          <h3 className="text-xl hidden md:flex font-semibold">Bookings</h3>
         </Link>
         <Link
           to={"/members"}
           className="flex gap-4 items-center hover:text-brand hover:font-bold "
         >
           <MdFamilyRestroom size={30} color="gray" />
-          <h3 className="text-xl font-semibold">My Family</h3>
+          <h3 className="text-xl hidden md:flex font-semibold">My Family</h3>
         </Link>
         <Link
           to={"/chats"}
           className="flex gap-4 items-center hover:text-brand hover:font-bold "
         >
           <IoMdChatboxes size={30} color="gray" />
-          <h3 className="text-xl font-semibold">My Chats</h3>
+          <h3 className="text-xl hidden md:flex font-semibold">My Chats</h3>
         </Link>
 
         <button
