51bd3327d552bb8414f2933c1ae229beee3a249a
Hamza Shadeez <hahmad1178@gmail.com>
2024-12-29 15:36:21 +0100

    update

--- src/pages/Appointments.jsx
+++ src/pages/Appointments.jsx
@@ -1,26 +1,45 @@
 import { Link } from "react-router-dom";
 import DashboardHeader from "../components/DashboardHeader";
 import { useEffect, useState } from "react";
-import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { collection, onSnapshot, query, where, orderBy, updateDoc, doc } from "firebase/firestore";
 import { db } from "../firebase";
 import { useRecoilState } from "recoil";
 import userData from "../lib/userData";
 import TimeAgo from "../components/TimeAgo";
+import LoadingComponent from "../components/LoadingComponent";
 
 const Appointments = () => {
   const [bookings, setBookings] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const [user, setUser] = useRecoilState(userData);
+
+  const Approve = async (id) => {
+    setLoading(true);
+    const bookingRef = doc(db, "bookings", id);
+    await updateDoc(bookingRef, { status: "approved" })
+      .then(() => {
+        setLoading(false);
+      })
+      .catch((error) => {
+        alert(error.message);
+        setLoading(false);
+      });
+  };
+
+
+  const Decline = async () => {};
 
   const getMyBookings = async () => {
     const q = query(
       collection(db, "bookings"),
-      where("doctorId", "==", user?.email)
+      where("doctorId", "==", user?.email),
+      orderBy("timestamp", "desc")
     );
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const bookingsList = [];
       querySnapshot.forEach((doc) => {
-        bookingsList.push(doc.data());
+        bookingsList.push({ docId: doc.id, ...doc.data()});
       });
       setBookings(bookingsList);
       console.log(bookingsList);
@@ -34,23 +53,26 @@ const Appointments = () => {
   return (
     <div>
       <DashboardHeader label="Appointments" />
-
-      <div className="p-4 md:p-10">
+      {loading && <LoadingComponent />}
+      <div className="p-4 md:p-10 grid grid-cols-1 md:grid-cols-2 gap-3">
         {bookings.map((booking) => (
-          <div className="flex flex-row gap-3 bg-white shadow-md p-4">
+          <div className="flex flex-row gap-4 bg-white shadow-md p-4">
             <img className="w-[70px] h-[70px] rounded-full" alt="patient" src={ booking?.patient?.profilePic ? booking?.patient?.profilePic : "/man.jpg"} />
             <section>
               <h1 className="text-lg font-semibold">Name: {booking?.patient?.fullName}</h1>
               <p className="text-sm text-gray-500">Email: {booking?.patient?.email}</p>
               <p className="text-gray-500 italic">Submitted <TimeAgo timestamp={booking?.timestamp} /></p>
-              <div className="flex flex-row gap-3 mt-3">
-                 <button className="bg-brand text-white px-4 py-2 rounded-md">
+              {booking?.status === "pending" && <div className="flex flex-row gap-3 mt-3">
+                 <button onClick={()=>Approve(booking?.docId)} className="bg-brand text-white px-4 py-2 rounded-md">
                     Approve
                  </button>
                  <button className="bg-red-600 text-white px-4 py-2 rounded-md">
                     Decline
                  </button>
-              </div>
+              </div>}
+
+              {booking?.status === "approved" && <div className="mt-3 bg-brand/20 text-brand py-2 rounded-md font-semibold text-sm text-center">Approved</div>}
+              {booking?.status === "declined" && <div className="mt-3 bg-red-500/20 text-red-500 py-2 rounded-md font-semibold text-sm text-center">Declined</div>}
             </section>
 
                 
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -8,6 +8,13 @@ const Landing = () => {
 
             </main>
 
+           <div className="">
+            <h1 className="text-3xl lg:text-5xl text-center font-semibold text-brand">Our Team</h1>
+           <section className="flex flex-col md:flex-row md:gap-12 gap-4">
+                <img src="/group.jpg" alt="" />
+                </section>
+           </div>
+
         </div>
     )
 }
