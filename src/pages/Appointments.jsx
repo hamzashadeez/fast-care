97ea521ba0ee2c209336d16be0f6a31e6d4439a2
Hamza Shadeez <hahmad1178@gmail.com>
2024-12-29 17:47:38 +0100

    commit

--- src/pages/Appointments.jsx
+++ src/pages/Appointments.jsx
@@ -1,7 +1,15 @@
 import { Link } from "react-router-dom";
 import DashboardHeader from "../components/DashboardHeader";
 import { useEffect, useState } from "react";
-import { collection, onSnapshot, query, where, orderBy, updateDoc, doc } from "firebase/firestore";
+import {
+  collection,
+  onSnapshot,
+  query,
+  where,
+  orderBy,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
 import { db } from "../firebase";
 import { useRecoilState } from "recoil";
 import userData from "../lib/userData";
@@ -12,7 +20,7 @@ const Appointments = () => {
   const [bookings, setBookings] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const [user, setUser] = useRecoilState(userData);
+  const [user, _] = useRecoilState(userData);
 
   const Approve = async (id) => {
     setLoading(true);
@@ -20,6 +28,7 @@ const Appointments = () => {
     await updateDoc(bookingRef, { status: "approved" })
       .then(() => {
         setLoading(false);
+        alert("Approved");
       })
       .catch((error) => {
         alert(error.message);
@@ -27,8 +36,19 @@ const Appointments = () => {
       });
   };
 
-
-  const Decline = async () => {};
+  const Decline = async (id) => {
+    setLoading(true);
+    const bookingRef = doc(db, "bookings", id);
+    await updateDoc(bookingRef, { status: "declined" })
+      .then(() => {
+        setLoading(false);
+        alert("Declined");
+      })
+      .catch((error) => {
+        alert(error.message);
+        setLoading(false);
+      });
+  };
 
   const getMyBookings = async () => {
     const q = query(
@@ -39,7 +59,7 @@ const Appointments = () => {
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const bookingsList = [];
       querySnapshot.forEach((doc) => {
-        bookingsList.push({ docId: doc.id, ...doc.data()});
+        bookingsList.push({ docId: doc.id, ...doc.data() });
       });
       setBookings(bookingsList);
       console.log(bookingsList);
@@ -55,32 +75,56 @@ const Appointments = () => {
       <DashboardHeader label="Appointments" />
       {loading && <LoadingComponent />}
       <div className="p-4 md:p-10 grid grid-cols-1 md:grid-cols-2 gap-3">
-        {bookings.map((booking) => (
-          <div className="flex flex-row gap-4 bg-white shadow-md p-4">
-            <img className="w-[70px] h-[70px] rounded-full" alt="patient" src={ booking?.patient?.profilePic ? booking?.patient?.profilePic : "/man.jpg"} />
+        {bookings.map((booking, index) => (
+          <div key={index} className="flex flex-row gap-4 bg-white shadow-md p-4">
+            <img
+              className="w-[70px] h-[70px] rounded-full"
+              alt="patient"
+              src={
+                booking?.patient?.profilePic
+                  ? booking?.patient?.profilePic
+                  : "/man.jpg"
+              }
+            />
             <section>
-              <h1 className="text-lg font-semibold">Name: {booking?.patient?.fullName}</h1>
-              <p className="text-sm text-gray-500">Email: {booking?.patient?.email}</p>
-              <p className="text-gray-500 italic">Submitted <TimeAgo timestamp={booking?.timestamp} /></p>
-              {booking?.status === "pending" && <div className="flex flex-row gap-3 mt-3">
-                 <button onClick={()=>Approve(booking?.docId)} className="bg-brand text-white px-4 py-2 rounded-md">
+              <h1 className="text-lg font-semibold">
+                Name: {booking?.patient?.fullName}
+              </h1>
+              <p className="text-sm text-gray-500">
+                Email: {booking?.patient?.email}
+              </p>
+              <p className="text-gray-500 italic">
+                Submitted <TimeAgo timestamp={booking?.timestamp} />
+              </p>
+              {booking?.status === "pending" && (
+                <div className="flex flex-row gap-3 mt-3">
+                  <button
+                    onClick={() => Approve(booking?.docId)}
+                    className="bg-brand text-white px-4 py-2 rounded-md"
+                  >
                     Approve
-                 </button>
-                 <button className="bg-red-600 text-white px-4 py-2 rounded-md">
+                  </button>
+                  <button
+                    onClick={() => Decline(booking?.docId)}
+                    className="bg-red-600 text-white px-4 py-2 rounded-md"
+                  >
                     Decline
-                 </button>
-              </div>}
+                  </button>
+                </div>
+              )}
 
-              {booking?.status === "approved" && <div className="mt-3 bg-brand/20 text-brand py-2 rounded-md font-semibold text-sm text-center">Approved</div>}
-              {booking?.status === "declined" && <div className="mt-3 bg-red-500/20 text-red-500 py-2 rounded-md font-semibold text-sm text-center">Declined</div>}
+              {booking?.status === "approved" && (
+                <div className="mt-3 bg-brand/20 text-brand py-2 rounded-md font-semibold text-sm text-center">
+                  Approved
+                </div>
+              )}
+              {booking?.status === "declined" && (
+                <div className="mt-3 bg-red-500/20 text-red-500 py-2 rounded-md font-semibold text-sm text-center">
+                  Declined
+                </div>
+              )}
             </section>
-
-                
           </div>
-
-
-
-
         ))}
       </div>
     </div>
